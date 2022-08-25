89f6f33ba7cb31fd67df44e4ce2408adc6b11c66
VTGCDantdga <69842621+Van11desu@users.noreply.github.com>
2022-08-26 02:15:55 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "wura!!";
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+        }
+    }
+}
